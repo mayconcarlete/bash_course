1ed4e6ca22e35aa2de8cf6d8571c554e0e200559
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-21 08:44:36 -0300

    feat: adicao no array pegando indice

--- 03-arrays-lista.sh
+++ 03-arrays-lista.sh
@@ -10,9 +10,15 @@ echo ${arr[1]}
 echo ${arr[2]}
 unset arr[2] # remove elemento
 echo ${arr[2]}
-echo ${arr[*]}
 # unset arr[*] remove todos os elementos
 
-
+# para acrescentar um item no array basta passar o indice e atribuir
+# arr[2]="Iara"
+# echo ${arr[*]}
+# para pegar o indice basta usar o comando.
+echo ${#arr[@]}
+index=$((${#arr[@]}+1))
+arr[index]="Simone Carlete"
+echo ${arr[*]}
 # Ps: para atribuir um array no terminal
 # basta variavel=(elemento1, elemento2 ...)
