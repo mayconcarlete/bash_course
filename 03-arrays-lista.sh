492438b395b90db17fcdf5f1f70cfc9fbecfd9d3
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-21 08:34:44 -0300

    docs: add comments

--- 03-arrays-lista.sh
+++ 03-arrays-lista.sh
@@ -2,7 +2,11 @@
 
 arr=("Maycon Carlete" "banana" "grape")
 
+echo ${arr}
 echo ${arr[0]}
 echo ${arr[1]}
 echo ${arr[2]}
 echo ${arr[*]}
+
+# Ps: para atribuir um array no terminal
+# basta variavel=(elemento1, elemento2 ...)
