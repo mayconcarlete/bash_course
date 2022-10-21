ba5e53a6c6e739a44e8d561f572070365d553d10
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-21 08:36:52 -0300

    docs: array

--- 03-arrays-lista.sh
+++ 03-arrays-lista.sh
@@ -5,8 +5,14 @@ arr=("Maycon Carlete" "banana" "grape")
 echo ${arr}
 echo ${arr[0]}
 echo ${arr[1]}
+arr[1]="Carolina Lima"
+echo ${arr[1]}
+echo ${arr[2]}
+unset arr[2] # remove elemento
 echo ${arr[2]}
 echo ${arr[*]}
+# unset arr[*] remove todos os elementos
+
 
 # Ps: para atribuir um array no terminal
 # basta variavel=(elemento1, elemento2 ...)
