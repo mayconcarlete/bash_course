cdb2486c4ac3d809203cc4d78a0e156f7975e882
Maycon Carlete <maycon.carlete@gmail.com>
2022-10-31 20:17:20 -0300

    feat: exercise 05

---
+++ shell-scripting-book/cap-01/exercise-05.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Write a shell script that displays
+# "man", "bear", "pig", "dog", "cat", "sheep"
+# on the screen with each appearing on a separated line.
+# Try to do this in a few lines as possible.
+# hint: Loops can be used to perform repetitive tasks.
+
+ANIMALS=("man bear pig dog cat sheep")
+for ANIMAL in ${ANIMALS[*]}
+do
+  echo $ANIMAL
+done
