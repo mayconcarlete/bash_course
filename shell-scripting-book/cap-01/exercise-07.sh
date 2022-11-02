318ea8b5da0be452d396170199806298c6b8062f
Maycon Carlete <maycon.carlete@gmail.com>
2022-11-02 09:01:20 -0300

    feat: exercise 7

---
+++ shell-scripting-book/cap-01/exercise-07.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Modify the previous script so that it accepts the file or
+# directory name as an argument instead of prompting the user to enter it.
+
+FILE=$1
+echo "file-directory name: ${FILE}"
+if [ -f "${FILE}" ]
+then
+  echo "${FILE} is a regular file."
+elif [ -d "${FILE}" ]
+then
+  echo "${FILE} is a directory."
+else
+  echo "${FILE} is something other than a regular file or a directory."
+fi
