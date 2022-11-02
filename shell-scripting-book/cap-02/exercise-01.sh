1f0d7fbccb73df890eec586bc5500ddaac09ad40
Maycon Carlete <maycon.carlete@gmail.com>
2022-11-02 09:23:39 -0300

    feat: exercise-01 cap 2

---
+++ shell-scripting-book/cap-02/exercise-01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Write a shell script that displays, "This script will exit with a 0 status."
+# Be sure that the script does indeed exist with a 0 exit status.
+
+echo "This script will exit with a 0 status."
+exit 0
